5076d2d381926e1155b5de2b53196a1ad5d43b22
AiryShift <religionofaperturelabs@gmail.com>
2015-05-07 11:39:15 +1000

    Add friendly exit to

--- MathsGame/Form1.Designer.vb
+++ MathsGame/Form1.Designer.vb
@@ -23,6 +23,8 @@ Partial Class mainScreen
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.friendlyExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -35,18 +37,41 @@ Partial Class mainScreen
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
+        'friendlyExit
+        '
+        Me.friendlyExit.Location = New System.Drawing.Point(1177, 647)
+        Me.friendlyExit.Name = "friendlyExit"
+        Me.friendlyExit.Size = New System.Drawing.Size(75, 23)
+        Me.friendlyExit.TabIndex = 1
+        Me.friendlyExit.Text = "Exit"
+        Me.friendlyExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(706, 495)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Choose a shape!"
+        '
         'mainScreen
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1264, 682)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.friendlyExit)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "mainScreen"
         Me.Text = "Welcome to Cool Maths Games!"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents friendlyExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
--- MathsGame/Form1.vb
+++ MathsGame/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class mainScreen
 
+    Private Sub friendlyExit_Click(sender As Object, e As EventArgs) Handles friendlyExit.Click
+        End
+    End Sub
 End Class
