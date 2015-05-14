4e04d66a96c845ddfb5f78ded6dde7ddd5221e76
AiryShift <religionofaperturelabs@gmail.com>
2015-05-14 12:26:05 +1000

    Modularize stuff into GlobalVariables

---
+++ MathsGame/GlobalVariables.vb
@@ -0,0 +1,15 @@
+﻿Public Class GlobalVariables
+    Public Const AREA_1 As String = "Circle"
+    Public Const AREA_2 As String = "Triangle"
+    Public Const AREA_3 As String = "Rectangle"
+    Public Const AREA_4 As String = "Trapezium"
+    Public Const AREA_5 As String = "Parallelogram"
+    Public Const AREA_6 As String = "Kite"
+
+    Public Const VOLUME_1 As String = "Cylinder"
+    Public Const VOLUME_2 As String = "Triangular Pyramid"
+    Public Const VOLUME_3 As String = "Rectangular Prism"
+    Public Const VOLUME_4 As String = "Sphere"
+    Public Const VOLUME_5 As String = "Triangular Prism"
+    Public Const VOLUME_6 As String = "Icosahedron"
+End Class
--- MathsGame/practice.vb
+++ MathsGame/practice.vb
@@ -1,33 +1,20 @@
 ﻿Public Class practice
-    Const AREA_1 As String = "Circle"
-    Const AREA_2 As String = "Triangle"
-    Const AREA_3 As String = "Rectangle"
-    Const AREA_4 As String = "Trapezium"
-    Const AREA_5 As String = "Parallelogram"
-    Const AREA_6 As String = "Kite"
-
-    Const VOLUME_1 As String = "Cylinder"
-    Const VOLUME_2 As String = "Triangular Pyramid"
-    Const VOLUME_3 As String = "Rectangular Prism"
-    Const VOLUME_4 As String = "Sphere"
-    Const VOLUME_5 As String = "Triangular Prism"
-    Const VOLUME_6 As String = "Icosahedron"
 
     Private Sub area_Click(sender As Object, e As EventArgs) Handles area.Click
-        shape1.Text = AREA_1
-        shape2.Text = AREA_2
-        shape3.Text = AREA_3
-        shape4.Text = AREA_4
-        shape5.Text = AREA_5
-        shape6.Text = AREA_6
+        shape1.Text = GlobalVariables.AREA_1
+        shape2.Text = GlobalVariables.AREA_2
+        shape6.Text = GlobalVariables.AREA_6
+        shape3.Text = GlobalVariables.AREA_3
+        shape4.Text = GlobalVariables.AREA_4
+        shape5.Text = GlobalVariables.AREA_5
     End Sub
 
     Private Sub volume_Click(sender As Object, e As EventArgs) Handles volume.Click
-        shape1.Text = VOLUME_1
-        shape2.Text = VOLUME_2
-        shape3.Text = VOLUME_3
-        shape4.Text = VOLUME_4
-        shape5.Text = VOLUME_5
-        shape6.Text = VOLUME_6
+        shape1.Text = GlobalVariables.VOLUME_1
+        shape2.Text = GlobalVariables.VOLUME_2
+        shape3.Text = GlobalVariables.VOLUME_3
+        shape4.Text = GlobalVariables.VOLUME_4
+        shape5.Text = GlobalVariables.VOLUME_5
+        shape6.Text = GlobalVariables.VOLUME_6
     End Sub
 End Class
