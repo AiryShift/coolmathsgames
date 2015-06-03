f87441fe0966713ed416932880c64d517e265679
AiryShift <religionofaperturelabs@gmail.com>
2015-06-03 22:31:42 +1000

    Add the opening of the correct shape practice form whenever the button is clicked in practice.vb

--- MathsGame/practice.vb
+++ MathsGame/practice.vb
@@ -32,8 +32,10 @@
     Private Sub shape1_Click(sender As Object, e As EventArgs) Handles shape1.Click
         If currentMode = SELECTED_AREA Then
             GlobalVariables.currentShape = GlobalVariables.AREA_1
+            pracCircle.Show()
         Else
             GlobalVariables.currentShape = GlobalVariables.VOLUME_1
+            pracCylinder.Show()
         End If
         Me.Hide()
     End Sub
@@ -41,8 +43,10 @@
     Private Sub shape2_Click(sender As Object, e As EventArgs) Handles shape2.Click
         If currentMode = SELECTED_AREA Then
             GlobalVariables.currentShape = GlobalVariables.AREA_2
+            pracTriangle.Show()
         Else
             GlobalVariables.currentShape = GlobalVariables.VOLUME_2
+            pracTriangularPrism.Show()
         End If
         Me.Hide()
     End Sub
@@ -50,8 +54,10 @@
     Private Sub shape3_Click(sender As Object, e As EventArgs) Handles shape3.Click
         If currentMode = SELECTED_AREA Then
             GlobalVariables.currentShape = GlobalVariables.AREA_3
+            pracRectangle.Show()
         Else
             GlobalVariables.currentShape = GlobalVariables.VOLUME_3
+            pracRectangularPrism.Show()
         End If
         Me.Hide()
     End Sub
@@ -59,8 +65,10 @@
     Private Sub shape4_Click(sender As Object, e As EventArgs) Handles shape4.Click
         If currentMode = SELECTED_AREA Then
             GlobalVariables.currentShape = GlobalVariables.AREA_4
+            pracTrapezium.Show()
         Else
             GlobalVariables.currentShape = GlobalVariables.VOLUME_4
+            pracSphere.Show()
         End If
         Me.Hide()
     End Sub
@@ -68,8 +76,10 @@
     Private Sub shape5_Click(sender As Object, e As EventArgs) Handles shape5.Click
         If currentMode = SELECTED_AREA Then
             GlobalVariables.currentShape = GlobalVariables.AREA_5
+            pracParallelogram.Show()
         Else
             GlobalVariables.currentShape = GlobalVariables.VOLUME_5
+            pracTriangularPrism.Show()
         End If
         Me.Hide()
     End Sub
@@ -77,8 +87,10 @@
     Private Sub shape6_Click(sender As Object, e As EventArgs) Handles shape6.Click
         If currentMode = SELECTED_AREA Then
             GlobalVariables.currentShape = GlobalVariables.AREA_6
+            pracKite.Show()
         Else
             GlobalVariables.currentShape = GlobalVariables.VOLUME_6
+            pracCone.Show()
         End If
         Me.Hide()
     End Sub
