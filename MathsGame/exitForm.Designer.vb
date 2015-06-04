4c7d1ee01a2e80d1f2002eb079eba7d86522c795
AiryShift <religionofaperturelabs@gmail.com>
2015-06-05 00:13:54 +1000

    Make exit pretty

--- MathsGame/exitForm.Designer.vb
+++ MathsGame/exitForm.Designer.vb
@@ -22,24 +22,25 @@ Partial Class exitForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(exitForm))
         Me.yes = New System.Windows.Forms.Button()
         Me.no = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'yes
         '
-        Me.yes.Location = New System.Drawing.Point(54, 56)
+        Me.yes.Location = New System.Drawing.Point(12, 12)
         Me.yes.Name = "yes"
-        Me.yes.Size = New System.Drawing.Size(75, 23)
+        Me.yes.Size = New System.Drawing.Size(244, 23)
         Me.yes.TabIndex = 0
         Me.yes.Text = "Yes"
         Me.yes.UseVisualStyleBackColor = True
         '
         'no
         '
-        Me.no.Location = New System.Drawing.Point(137, 85)
+        Me.no.Location = New System.Drawing.Point(262, 12)
         Me.no.Name = "no"
-        Me.no.Size = New System.Drawing.Size(75, 23)
+        Me.no.Size = New System.Drawing.Size(221, 23)
         Me.no.TabIndex = 1
         Me.no.Text = "No"
         Me.no.UseVisualStyleBackColor = True
@@ -49,9 +50,10 @@ Partial Class exitForm
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.MathsGame.My.Resources.Resources.MapleStory_SVG
-        Me.ClientSize = New System.Drawing.Size(295, 275)
+        Me.ClientSize = New System.Drawing.Size(495, 43)
         Me.Controls.Add(Me.no)
         Me.Controls.Add(Me.yes)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "exitForm"
         Me.Text = "Are you sure that you want to go?"
         Me.ResumeLayout(False)
